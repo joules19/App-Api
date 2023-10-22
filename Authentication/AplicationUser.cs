04bc8a4a154b00dffa331f09035f89aaf3655e00
joules19 <babafemiayodele@gmail.com>
2023-10-22 21:36:59 +0100

    latest update

--- Authentication/AplicationUser.cs
+++ Authentication/AplicationUser.cs
@@ -9,7 +9,6 @@ namespace api_app.Authentication
     {
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
-
         public string? StreetAddress { get; set; }
         public string? City { get; set; }
         public string? State { get; set; }
